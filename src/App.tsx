bab0f32736076ec0d44023292239800b85b805bb
angganurf <angganurfaizal234@gmail.com>
2025-08-19 22:18:50 +0700

    change CA

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ function App() {
 		}
 	};
 
-	const contractAddress = "9eKgXLJSPDLHWui5sbqKgvTbxh4FNusbc3Bho6ZrBAGS";
+	const contractAddress = "Coming soon ...!";
 
 	const imageUrls = [
 		"/assets/1.gif",
