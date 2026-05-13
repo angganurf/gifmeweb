15fd80188a42e7eca8f506e2abbae27044609540
Angga Nurfaisal <31128877+angganurf@users.noreply.github.com>
2026-05-13 13:56:20 +0700

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 	const [isViewerOpen, setIsViewerOpen] = useState(false);
 	const [currentIndex, setCurrentIndex] = useState<number>(0);
 
-	const contractAddress = "soon...";
+	const contractAddress = "EgGp5w3nMxrZcd3xKUc5fvNHXCJasyiukJcejbYXpump";
 
 	const imageUrls = [
 		"/assets/1.gif",
